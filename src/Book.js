486f7a069ee3859981d7d2d3fc82f3877685cc0f
Nektarios <nektario@gmail.com>
2019-02-23 14:32:54 -0500

    initial layout

---
+++ src/Book.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Book.css'
+
+function Book() {
+    return (
+        <div className='book-card d-flex'>
+        
+
+        </div>
+    )
+}
+
+export default Book
---
+++ src/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SearchBar.css'
+
+function SearchBar() {
+    return (
+         <input id='search-bar' placeholder='Search Books'></input>
+    )
+}
+
+export default SearchBar
