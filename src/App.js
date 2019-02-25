4fd55298e0e0d5bd39739776bcd14797447c3e5e
nektario <nektario@gmail.com>
2019-02-25 11:59:40 -0500

    More responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SearchBar from './SearchBar'
 import BookList from './BookList'
-import './App.css';
+import './App.scss';
 
 const API_URL = 'https://www.googleapis.com/books/v1/volumes?printType=books&startIndex=0&maxResults=10&fields=kind,totalItems,items(id,selfLink,volumeInfo(title,authors,publisher,publishedDate,description,imageLinks/thumbnail,previewLink))'
 const API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY
@@ -11,7 +11,8 @@ function App() {
 
     function handleSearch(searchTerms) {
         const query = searchTerms.split(' ').join('+')
-        const url = `${API_URL}&key=${API_KEY}&q=${query}`
+        //const url = `${API_URL}&key=${API_KEY}&q=${query}`
+        const url = `${API_URL}&q=${query}`
 
         fetch(url)
             .then(response => response.json())
@@ -26,7 +27,8 @@ function App() {
     return (
         <div className='App d-flex flex-col'>
             <div id='container' className='d-flex flex-col'>
-                <header>
+                <header className='d-flex flex-col flex-center'>
+                    <h1 id='title'>Book Search</h1>
                     <SearchBar onSearch={handleSearch}></SearchBar>
                 </header>
 
